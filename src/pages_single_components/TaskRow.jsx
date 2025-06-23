5e45b297a8468712fb273dc92d6775299f10c662
penso996 <penso.andrea@outlook.it>
2025-06-23 18:06:23 +0200

    added color todo/doing/done

--- src/pages_single_components/TaskRow.jsx
+++ src/pages_single_components/TaskRow.jsx
@@ -4,7 +4,20 @@ export default function TaskRow({ task }) {
     return (
         <tr>
             <td>{task.title}</td>
-            <td>{task.status}</td>
+            <td
+                style={{
+                    color:
+                        task.status === "To do"
+                            ? "red"
+                            : task.status === "Doing"
+                                ? "orange"
+                                : task.status === "Done"
+                                    ? "green"
+                                    : "black"
+                }}
+            >
+                {task.status}
+            </td>
             <td>{new Date(task.createdAt).toLocaleDateString("it-IT")}</td>
         </tr>
     )
