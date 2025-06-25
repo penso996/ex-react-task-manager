86f368c3a41b8086ba101201c468a1b67b77e57a
penso996 <penso.andrea@outlook.it>
2025-06-25 13:41:36 +0200

    added validation for inputs + style

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,27 +1,52 @@
 // Import hooks from React
-import { useRef, useState } from "react"
+import { useMemo, useRef, useState } from "react"
 
 
 export default function AddTask() {
 
+    const symbols = "!@#$%^&*()-_=+[]£{}|;:'\",.<>?/`~";
+
     const [taskTitle, setTaskTitle] = useState("");
     const descriptionRef = useRef();
     const statusRef = useRef();
+
+    const taskTitleError = useMemo(() => {
+        if (!taskTitle.trim())
+            return "Can't be empty"
+        if ([...taskTitle].some(char => symbols.includes(char)))
+            return "No special characters"
+        return "";
+    }, [taskTitle])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (taskTitleError)
+            return;
+        const newTask = {
+            title: taskTitle.trim(),
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+        console.log(newTask);
+    }
 
     // RENDER
     return (
         <>
             <h1>ADD TASK</h1>
 
-            <form>
-                {/* task name */}
+            <form onSubmit={handleSubmit}>
+                {/* task title */}
                 <label>
-                    Task Name:
+                    Task title:
                     <input
                         type="text"
                         value={taskTitle}
                         onChange={e => setTaskTitle(e.target.value)}
                     />
+                    {taskTitleError && (
+                        <p style={{ color: 'red' }}>{taskTitleError}</p>
+                    )}
                 </label>
                 {/* task description */}
                 <label>
@@ -40,6 +65,13 @@ export default function AddTask() {
                         ))}
                     </select>
                 </label>
+                {/* submit button */}
+                <button
+                    type="submit"
+                    disabled={taskTitleError}
+                >
+                    Add Task
+                </button>
             </form>
         </>
     )
